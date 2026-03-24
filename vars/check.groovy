620fd9d2eb4a40d16cd727435dd9bacb52df86a9
SunChip <fear.kaz@gmail.com>
2026-03-24 15:39:37 +0300

    add correct path

--- vars/check.groovy
+++ vars/check.groovy
@@ -1,14 +1,16 @@
 def checkStage() {
     stage('Check services in kubernetes') {
-        withCredentials([file(credentialsId: 'kubeconfig', variable: 'KUBECONFIG')]) {
-            echo "Check service in kubernetes"
+        withEnv(["PATH=/usr/local/bin:/opt/homebrew/bin:$PATH"]) {
+            withCredentials([file(credentialsId: 'kubeconfig', variable: 'KUBECONFIG')]) {
+                echo "Check service in kubernetes"
 
-            def checkDeploy = sh (
-                script: "kubectl get deploy",
-                returnStdout: true
-            ).trim()
-            if(!checkPrcheckDeployometheus.contains("prometheus-deployment") &&!checkPrcheckDeployometheus.contains("pushgateway")){
-                error("Services isn't in kuber")
+                def checkDeploy = sh (
+                    script: "kubectl get deploy",
+                    returnStdout: true
+                ).trim()
+                if(!checkPrcheckDeployometheus.contains("prometheus-deployment") &&!checkPrcheckDeployometheus.contains("pushgateway")){
+                    error("Services isn't in kuber")
+                }
             }
         }
 
